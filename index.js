a0a16398406fa520efd5fccc24f065f1ff986842
Aria Stewart <aredridel@dinhe.net>
2015-04-03 16:10:17 -0400

    Make it so

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var bcp47 = require('bcp47');
+
+module.exports = {
+    languagePackPath: languagePackPath,
+    middleware: function() {
+        return function (req, res, next) {
+            if (!res.locals.makara) res.locals.makara = {};
+            res.locals.makara.languagePackPath = languagePackPath(res.locals.locale);
+            next();
+        };
+    }
+
+};
+
+function languagePackPath(locale) {
+    if (!locale) throw new Error("Must specify a locale");
+    // Handle PayPal-style input
+    if (locale.language && locale.country) locale = locale.language + '-' + locale.country;
+
+    // Handle strings
+    if (typeof locale == 'string') locale = bcp47.parse(locale);
+
+    if (!locale || !locale.langtag || !locale.langtag.language || !locale.langtag.language.language || !locale.langtag.region) throw new Error('Invalid locale');
+
+    return locale.langtag.language.language + '-' + locale.langtag.region + '/_languagepack.js';
+}
